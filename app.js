7a35becbc850a98459106e30dfa25a6300673eb7
SuraHorani <>
2021-06-02 13:24:11 +0000

    change

---
+++ app.js
@@ -0,0 +1,9 @@
+alert('YOUR HEALTH MATTERS!')
+
+let start= prompt('ARE YOU READY TO START YOUR JOURNEY?')
+if (start =="yes") {
+  alert("we are ready to help you")
+  }
+  else {
+    alert("never lose hope")
+  }
