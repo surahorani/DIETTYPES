4f5a4d8862cd676a81235fa7ffee808aa3906eb0
SuraHorani <>
2021-06-03 13:38:35 +0000

    diets

--- app.js
+++ app.js
@@ -7,3 +7,14 @@ if (start =="yes") {
   else {
     alert("never lose hope")
   }
+let diet=prompt('which diet you prefer?')
+if (diet=='keto'){document.write('<img src="https://i.insider.com/5afae25d5e48ec9d0c8b4678?width=600&format=jpeg&auto=webp"/>')}
+
+let budget=prompt("how much is your budget?(1-8)")
+console.log(budget)
+while(!budget){
+  budget=prompt("how much is your budget?(1-8)")
+}
+for (let i=0 ; i <budget ;i++){
+  document.write('<img src="https://fathom-news.com/wp-content/uploads/2021/03/02-Blog-Healthy-Food-L.jpg"')
+}
