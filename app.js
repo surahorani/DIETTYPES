adc8b014f238e33ed13b1ad397bb87d736a1d6f5
SuraHorani <>
2021-06-06 10:44:50 +0000

    fun

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+function  alerting (){
 alert('YOUR HEALTH MATTERS!')
-
 let start= prompt('ARE YOU READY TO START YOUR JOURNEY?')
-if (start =="yes") {
-  alert("we are ready to help you")
-  }
-  else {
-    alert("never lose hope")
-  }
+if (start =="yes") { alert("we are ready to help you") }
+else {alert("never lose hope")} }
+
+function note() {
+alert ("our prices suit everyone!")
+let answer = prompt ("how much is your budget?")
+var correctAnswer = "40$"
+while (answer != correctAnswer){answer = prompt ("how much is your budget?") }
+var sun = prompt('how many meals do you want?')
+
+for (let i =0; i<sun;i++){
+ document.write("<img src='https://alumniyat.com/wp-content/uploads/2020/07/DASH-diet.jpg' />" )
+} }
+
+alerting()
+note()
